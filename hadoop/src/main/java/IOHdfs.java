e279b1b353cc420a60cebd6779a4a05303f1c5d3
Neel <neel783d@gmail.com>
2021-07-07 22:19:47 +0900

    read using readHdfsFileUsingFsDataInputStream

--- hadoop/src/main/java/IOHdfs.java
+++ hadoop/src/main/java/IOHdfs.java
@@ -2,6 +2,7 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.apache.hadoop.fs.Path;
@@ -29,6 +30,21 @@ public class IOHdfs {
     }
   }
 
+  private void readHdfsFileUsingFsDataInputStream(String fileUrl) throws Exception {
+    // Reads an hdfs file and print to System.out
+    FSDataInputStream in = null;
+    Configuration conf = new Configuration();
+    FileSystem fs = FileSystem.get(URI.create(fileUrl), conf);
+
+    try {
+      in = fs.open(new Path(fileUrl));
+      in.seek(0);
+      IOUtils.copyBytes(in, System.out, 4096, false);
+    } finally {
+      IOUtils.closeStream(in);
+    }
+  }
+
   private void readHdfsFileUrlStream(String fileUrl) throws Exception {
     // Reads an hdfs file and print to System.out
     InputStream in = null;
@@ -42,18 +58,18 @@ public class IOHdfs {
 
   public static void main(String[] args) throws Exception {
     String fp = "hdfs://localhost/neel/pom.xml";
-    int choice = 2;
+    int choice = 3;
     IOHdfs ioHdfs = new IOHdfs();
 
     switch (choice) {
       case 2:
         ioHdfs.readHdfsFileUsingFileSystem(fp);
         break;
+      case 3:
+        ioHdfs.readHdfsFileUsingFsDataInputStream(fp);
+        break;
       default:
         ioHdfs.readHdfsFileUrlStream(fp);
     }
-
-
-
   }
 }
